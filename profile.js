0319b904b5547e99830b47f95cf06249599fb32d
O. C. Christian <chilancer@gmail.com>
2022-06-24 13:07:16 +0100

    initial commit

---
+++ profile.js
@@ -0,0 +1,6 @@
+use strict;
+
+let myName = "Christian";
+let myHeight = 187;
+let myCountry = "Nigeria";
+console.log("My name is " + myName + ". I am " + myHeight + "cm tall, and I was born in " + myCountry + ".");
